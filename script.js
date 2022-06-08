2110b624d6582130c02542a95e77ec68065d9688
feet <weldersilvasurf@gmail.com>
2022-06-08 20:45:13 -0300

    variavel

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome= "welder";
+console.log(nome);
+
+let idade = 32;
+console.log(idade);
+
+
+let ano = 2022;
+console.log(ano - idade);
+
+console.log(ano + idade);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
